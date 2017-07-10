9106ad0f744fede0baeecd87b9a4b1f9538429e9
Logan Crewss <logancrewss@Blakes-MacBook-Air.local>
2017-07-10 14:38:32 -0600

    updated port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var server = express()
 var logan = require('./logan.json')
-
+var port = process.env.PORT || 8000
 server.get('/Logan', function (request,response) {
   response.json(logan)
 })
@@ -15,4 +15,4 @@ server.get('*', function(request, response){
   response.send('Dude,Wrong page')
 })
 
-server.listen(8000)
+server.listen(port)
